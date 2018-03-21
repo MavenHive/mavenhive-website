cf198a94e81b2b59f3de3ebed6ebbfc3f95bbda8
praveen <apnerve@gmail.com>
2018-03-21 12:48:04 +0530

    Added honeypot field for controlling spam

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,7 @@ export default () => (
     </div>
     <div className='mw-mavenhive center ph4 mt6 mb5'>
       <h2 className='f3 normal mt0 mb4 pb3'>Please add your project details, we will get back to you</h2>
-      <form className='mw6 tracked-mh' method='POST' netlify>
+      <form className='mw6 tracked-mh' method='POST'data-netlify="true" data-netlify-honeypot="bot-field">
         <div className='h3 mv4'>
           <label className='db f5 mb1'>Company/Project Name</label>
           <input className='h-75 w-100 pa3 ba br2 b--moon-gray' type='text' />
@@ -41,6 +41,11 @@ export default () => (
           <label className='db f5 mb1'>What do you need help with? *</label>
           <textarea className='h2 w-100 mw-100 pa3 h4 ba br2 b--moon-gray' />
         </div>
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" />
+          </label>
+        </p>
         <button className='f-1 bg-brand br2 white pv2 ph4 tc bw0'>Submit</button>
       </form>
     </div>
