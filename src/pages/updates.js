ccf2f5c997aa9971ffe40f15aeeb3506e7551f85
praveen <apnerve@gmail.com>
2018-03-28 23:19:12 +0530

    Working udpates page

--- src/pages/updates.js
+++ src/pages/updates.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import updates from '../content/updates.json'
 
 import { Talk, Blog, Event } from '../components/Updates'
 
+console.log(updates)
+
 const NotFoundPage = () => (
   <div className="mw-mavenhive ph4 center mt5 pt3">
-    <Helmet title="Event/Blog/Talk Updates | MavenHive" />
+    <Helmet title={`${updates.title} | MavenHive`} />
     <h1 className="mb5 pb4">
       &ldquo;What our team has been upto?&hellip;&rdquo;
     </h1>
 
-    <h2 className="f3 brand mb4">Events/Blog/Talk Updates</h2>
+    <h2 className="f3 brand mb4">{updates.title}</h2>
 
     <div className="mb5">
       <Talk
         title="RubyConf India 2013 Write your ruby in Style"
         author="Bhavin Javia"
         url="https://youtu.be/J03pbYLeO_A"
+      />
+      <Event
+        title="We are Silver Sponsors @ RubyConf India"
+        content="2018 RubyConf India is going to be at Namma Bengaluru, Welcome! The Chancery Pavilion Hotel, Bengaluru. 9-10 February 2018"
+        author="bhavin"
+        url="http://rubyconfindia.org/#sponsor"
       />
       <Blog
         title="Javascript Object, Prototype and Inheritance"
